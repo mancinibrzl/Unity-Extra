830147a23cebe1d164b3fe08542d1fc32e76f77f
mancinibrzl <mariomancinineto2017@gmail.com>
2024-09-09 17:26:12 -0300

    Update ManciniUtils.cs

--- UNITY EXTRA/Assets/Scripts/Utils/ManciniUtils.cs
+++ UNITY EXTRA/Assets/Scripts/Utils/ManciniUtils.cs
@@ -4,9 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Random = UnityEngine.Random;
+//using UnityEditor;
 
 public static class ManciniUtils
 {
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Mancini Menu/Test")]
+    public static void Test()
+    {
+        Debug.Log("Test");
+    }
+
+    [UnityEditor.MenuItem("Mancini Menu/Test 2 %g")]
+    public static void Test2()
+    {
+        Debug.Log("Test2");
+    }
+
+#endif
+
     public static T GetRandom<T>(this T[] array)
     {
         if (array.Length == 0)
